e34716818ff87122e37abe424629d73ea2fc9faf
Josh <jocjohns@mtu.edu>
2016-05-19 23:37:25 -0400

    Text now sizes right on start. Added chat opacity and chat/video overlap.

--- inject/inject.js
+++ inject/inject.js
@@ -1,18 +1,21 @@
 
 $(document).ready(function(){
   if(localStorage.getItem("enabled") === null) localStorage["enabled"] = "true";
+  if(localStorage.getItem("chatOverlap") === null) localStorage["chatOverlap"] = "false";
+  if(localStorage.getItem("chatOpacity") === null) localStorage["chatOpacity"] = 100;
   if(localStorage.getItem("chatLocation") === null) localStorage["chatLocation"] = "r";
   if(localStorage.getItem("chatWidth") === null) localStorage["chatWidth"] = 250;
   if(localStorage.getItem("chatFontSize") === null) localStorage["chatFontSize"] = 10;
 
   var enabled = (localStorage["enabled"] == "true");
+  var chatOverlap = (localStorage["chatOverlap"] == "true");
+  var chatOpacity = localStorage["chatOpacity"];
   var chatLocation = localStorage["chatLocation"];
   var chatWidth = localStorage["chatWidth"];
   var chatFontSize = localStorage["chatFontSize"];
 
   //preset settings
   settingsHeight = (enabled ? 160 : 30);
-  console.log("STARTING: enabled = " + enabled);
 
   if(enabled){
     var overlaySettings = `
@@ -29,8 +32,8 @@ $(document).ready(function(){
        z-index: 101;
     `;
     var overlay = "<div id='overlay' style='" + overlaySettings + "'>";
-    $("body").append(overlay);
-    console.log("INPUT OVERLAY");
+    //$("body").append(overlay);
+    //console.log("INPUT OVERLAY");
   }
 
   //conditions
@@ -42,9 +45,10 @@ $(document).ready(function(){
     "#left_col": "display: none;",
     "#left_close": "display: none;",
     //video and stats display
-    "#main_col": "margin-left: " + (chatLocation == "r" ? 0 : chatWidth) + "px; margin-right: " + (chatLocation == "r" ? chatWidth : 0) + "px; background-color: #000000;",
-    "#right_col": "width: " + chatWidth + "px;" + (chatLocation == "r" ? "" : " left: 0px;"),
+    "#main_col": "margin-left: " + (chatLocation == "r" || chatOverlap ? 0 : chatWidth) + "px; margin-right: " + (chatLocation == "l"  || chatOverlap ? 0 : chatWidth) + "px; background-color: #000000;",
+    "#right_col": "width: " + chatWidth + "px;" + (chatLocation == "r" ? "" : " left: 0px;") + "opacity: " + (chatOpacity / 100.0) + ";",
     ".tse-scroll-content": "width: " + chatWidth + "px; overflow: hidden;",
+    ".app-main": "background: #000000;",
     "#main_col .tse-scrollbar": "display: none !important;",
     //video title and user info
     "#broadcast-meta": "margin: 0px;",
@@ -64,7 +68,7 @@ $(document).ready(function(){
     ".stats-and-actions": "padding: 0 10px;",
 
     //video
-    ".dynamic-player": "width: " + ($(window).width() - chatWidth) + "px !important; height: " + ($(window).height() - 30) + "px !important;",
+    ".dynamic-player": "width: " + ($(window).width() - (chatOverlap ? 0 : chatWidth)) + "px !important; height: " + ($(window).height() - 30) + "px !important;",
 
     //CHAT STUFF
     "#right_close": "display: none;",
@@ -78,6 +82,7 @@ $(document).ready(function(){
     ".chat-line": "padding: 0; line-height: 17px",
     ".emoticon": "height: 15px; margin: " + (chatFontSize - 8) + "px;",
     ".conversations-content": "display: none;",
+    ".ember-chat": "min-width: 0px",
     //sendchat stuff
     ".textarea-contain": "margin-bottom: 3px;",
 
@@ -121,6 +126,7 @@ $(document).ready(function(){
       position: relative;
       top: 2px;
       margin-bottom: 10px;
+      height: 10px;
     `;
     var labelStyle = `
       display: inline;
@@ -128,7 +134,19 @@ $(document).ready(function(){
     var selectStyle = `
       display: inline;
       font-size: 10px;
+      margin-bottom: 5px;
+    `;
+    var fieldStyle = `
+      width: 100%;
+      display: inline;
+      color: #ffffff;
+      border: solid 1px #dddddd;
+      font-size: 10px;
       margin-bottom: 10px;
+    `;
+    var rangeStyle = `
+      width: 50px;
+      height: 10px;
     `;
     var settingsStyle = `
       height: ` + settingsHeight + `px;
@@ -143,11 +161,17 @@ $(document).ready(function(){
     var settings = `
     <div class='settings' style='` + settingsStyle + `'>
       Enabled: <input class="enabled" style='` + checkStyle + `' type='checkbox'><br/>
-      Chat Location: <select class="chatLocation" style='` + selectStyle + `'><option value="r">Right</option><option value="l">Left</option></select><br/>
-      Chat Font Size: <label class='chatFontSizeNum' style='` + labelStyle + `'>0</label><br/>
-      <input class='chatFontSizeRange' type='range' min='4' max='20'><br/>
-      Chat Width: <label class='chatWidthNum' style='` + labelStyle + `'>0</label><br/>
-      <input class='chatWidthRange' type='range' min='175' max='500'>
+      <fieldset style='` + fieldStyle + `'>
+        <legend>Chat</legend>
+        Location: <select class="chatLocation" style='` + selectStyle + `'><option value="r">Right</option><option value="l">Left</option></select><br/>
+        Overlap Video: <input class="chatOverlap" style='` + checkStyle + `' type='checkbox'><br/>
+        Opacity: <label class='chatOpacityNum' style='` + labelStyle + `'>0</label>
+        <input class='chatOpacityRange' style='` + rangeStyle + `' type='range' min='10' max='100'><br/>
+        Font Size: <label class='chatFontSizeNum' style='` + labelStyle + `'>0</label>
+        <input class='chatFontSizeRange' style='` + rangeStyle + `' type='range' min='4' max='20'><br/>
+        Width: <label class='chatWidthNum' style='` + labelStyle + `'>0</label>
+        <input class='chatWidthRange' style='` + rangeStyle + `' type='range' min='175' max='500'>
+      </fieldset>
     </div>`;
 
     //if not enabled, only show checkbox
@@ -164,6 +188,9 @@ $(document).ready(function(){
 
     //set settings values
     $("#right_col").append(settings);
+    if(chatOverlap) $(".chatOverlap").prop("checked", true);
+    $(".chatOpacityNum").text(localStorage["chatOpacity"]);
+    $(".chatOpacityRange").val(localStorage["chatOpacity"]);
     $(".chatFontSizeNum").text(localStorage["chatFontSize"]);
     $(".chatFontSizeRange").val(localStorage["chatFontSize"]);
     $(".chatWidthNum").text(localStorage["chatWidth"]);
@@ -213,8 +240,27 @@ $(document).ready(function(){
     $("#right_col").css({"left": (chatLocation == "r" ? "auto" : "0px"), "right": (chatLocation == "r" ? "0px" : "auto")});
   });
 
+  //overlap checkbox change
+  $(document).on("change", ".chatOverlap", function(){
+    localStorage["chatOverlap"] = ($(this).prop("checked") == true) ? "true" : "false";
+    chatOverlap = localStorage["chatOverlap"];
+    if(localStorage["chatOverlap"] == true){
+      $("#main_col").css({"margin-left": "0px", "margin-right": "0px"});
+    }else{
+      $("#main_col").css({"margin-left": (chatLocation == "r" ? 0 : localStorage["chatWidth"]) + "px", "margin-right": (chatLocation == "r" ? localStorage["chatWidth"] : 0) + "px"});
+    }
+  });
+
+  //opacity range change
+  $(document).on("input", ".chatOpacityRange", function(){
+    localStorage["chatOpacity"] = $(".chatOpacityRange").val();
+    chatOpacity = parseInt(localStorage["chatOpacity"]);
+    $(".chatOpacityNum").text(chatOpacity);
+    $(".right_col").css({'opacity': (chatOpacity / 100.0)});
+  });
+
   //font range change
-  $(document).on("change", ".chatFontSizeRange", function(){
+  $(document).on("input", ".chatFontSizeRange", function(){
     localStorage["chatFontSize"] = $(".chatFontSizeRange").val();
     chatFontSize = parseInt(localStorage["chatFontSize"]);
     $(".chatFontSizeNum").text(chatFontSize);
@@ -227,22 +273,22 @@ $(document).ready(function(){
     localStorage["chatWidth"] = $(".chatWidthRange").val();
     chatWidth = parseInt(localStorage["chatWidth"]);
     $(".chatWidthNum").text(chatWidth);
-    $("#main_col").css({"margin-left": (localStorage["chatLocation"] == "r" ? 0 : chatWidth) + "px", "margin-right": (localStorage["chatLocation"] == "r" ? chatWidth : 0) + "px"});
+    $("#main_col").css({"margin-left": (localStorage["chatLocation"] == "r" || chatOverlap ? 0 : chatWidth) + "px", "margin-right": (localStorage["chatLocation"] == "l"  || chatOverlap ? 0 : chatWidth) + "px"});
     $(".tse-scroll-content").css({"width": chatWidth + "px"});
-    $(".dynamic-player").css({"width": ($(window).width() - chatWidth) + "px"});
+    $(".dynamic-player").css({"width": ($(window).width() - (chatOverlap ? 0 : chatWidth)) + "px"});
     $("#right_col").css({"width": chatWidth + "px"});
     $(".chat-room").css({"width": chatWidth + "px"});
     $(".chat-interface").css({"width": chatWidth + "px"});
     $(".settings").css({"width": chatWidth + "px"});
 
-    var newStyle = "width: " + ($(window).width() - chatWidth) + "px !important; height: " + ($(window).height() - 30) + "px !important;";
+    var newStyle = "width: " + ($(window).width() - (chatOverlap ? 0 : chatWidth)) + "px !important; height: " + ($(window).height() - 30) + "px !important;";
     $(".dynamic-player").attr("style", $(".dynamic-player").attr("style") + "; " + newStyle);
   });
 
   //new chat message received
   $(".chat-lines").bind("DOMSubtreeModified",function(){
     if(enabled){
-      $(".chat-line:not([data-changed])").attr("style", $(".chat-line").attr("style") + "; " + "padding: 0; line-height: " + (parseInt(localStorage["chatFontSize"]) + 7 + (parseInt(localStorage["chatFontSize"]) / 4)) + "px").attr("data-changed", "1");
+      $(".chat-line:not([data-changed])").attr("style", $(".chat-line").attr("style") + "; font-size: " + chatFontSize + "px; " + "padding: 0; line-height: " + (parseInt(localStorage["chatFontSize"]) + 7 + (parseInt(localStorage["chatFontSize"]) / 4)) + "px").attr("data-changed", "1");
       $(".emoticon:not([data-changed])").attr("style", $(".emoticon").attr("style") + "; " + "height: " + (parseInt(localStorage.getItem("chatFontSize")) + 7) + "px; margin: 0px;").attr("data-changed", "1");
     }
   });
@@ -250,7 +296,7 @@ $(document).ready(function(){
   //resize video on window resize
   $(window).on("resize", function(){
     if(enabled){
-      var newStyle = "width: " + ($(window).width() - chatWidth) + "px !important; height: " + ($(window).height() - 30) + "px !important;";
+      var newStyle = "width: " + ($(window).width() - (chatOverlap ? 0 : chatWidth)) + "px !important; height: " + ($(window).height() - 30) + "px !important;";
       $(".dynamic-player").attr("style", $(".dynamic-player").attr("style") + "; " + newStyle);
 
       $(".rightcol-content").css({"height": $(window).height() - (settingsShown ? settingsHeight : 0)});
